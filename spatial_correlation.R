521be4e68d68f5ca6c8d5c0089d7f15bd206be84
Enan Ajmain <3nan.ajmain@gmail.com>
2021-05-03 15:50:38 +0600

    Add: save plots in pdf

--- spatial_correlation.R
+++ spatial_correlation.R
@@ -4,6 +4,10 @@ library(SingleCellExperiment)
 library(scater)
 library(scran)
 
+require(ggforce)
+require(patchwork)
+require(ggpubr)
+
 if (!file.exists("output")) {
   system("mkdir -p output")
 }
@@ -95,113 +99,103 @@ clusterGenePair <- sapply(clusterNames, function(i) {
       clusterGenePair[[i]] <- clusterData[clusterData[,6]==i, 7]
 })
 
-# genes <- c("Cst3", "B2m", "Ier5", "Cst3", "Cst3", "Tmsb4x", "B2m", "Actb")
-genes <- c("Dcx", "Nrep", "Tubb2b", "Crmp1", "Dlx2", "Bcl11b", "Meis2", "Sp9", "Cpne4", "Synpr", "Tuba1a", "Nsg2", "Map1b", "Nrxn3", "Tubb5", "Eif4a1")
 
-pairCount <- as.matrix(rbind(counts[genes,]))
-rownames(pairCount) <- genes
+if (!file.exists("output/p_val_plots")) {
+  system("mkdir output/p_val_plots")
+}
+
+# t = theme(legend.key.width = unit(0.5, "inches")) +
+#     theme(plot.title = element_text(size = 20)) +
+#     theme(axis.title = element_text(size = 15))
+
+plot_wcor = function(df_res) {
+  ggplot(df_res, aes(x = -x, y = y)) +
+    geom_point(aes(colour = wcor), size = 5) +
+    theme_minimal() +
+    theme(panel.grid = element_blank()) +
+    theme(axis.text = element_blank()) +
+    xlab("") +
+    ylab("") +
+    labs(colour = "") +
+    theme(legend.position = "bottom") +
+    theme(plot.title = element_text(hjust = 0.5, face = "italic")) +
+    scale_color_viridis_c(breaks = c(0,max(df_res$wcor)),
+                          limits = c(0,max(df_res$wcor)),
+                          labels = c("Low","High")) +
+    coord_fixed() +
+    guides(colour = guide_colourbar(title.position = "top",
+                                    title.hjust = 0.5)) +
+    theme(legend.title=element_text(size=15)) +
+    labs(colour = "Weighted Correlation") +
+    NULL
+}
+
+plot_pvals = function(df_res) {
+  ggplot(df_res, aes(x = -x, y = y)) +
+    # geom_point(aes(colour = pvals), size = 5) +
+    geom_point(aes(colour = -log10(pvals)), size = 5) +
+    theme_minimal() +
+    theme(panel.grid = element_blank()) +
+    theme(axis.text = element_blank()) +
+    xlab("") +
+    ylab("") +
+    labs(colour = "") +
+    theme(legend.position = "bottom") +
+    theme(plot.title = element_text(hjust = 0.5, face = "italic")) +
+    scale_color_viridis_c(breaks = c(0,max(df_res$pvals)),
+                          limits = c(0,max(df_res$pvals)),
+                          labels = c("Low","High")) +
+    coord_fixed() +
+    guides(colour = guide_colourbar(title.position = "top",
+                                    title.hjust = 0.5)) +
+    theme(legend.title=element_text(size=15)) +
+    labs(colour = "-log(pval)") +
+    NULL
+}
+
+for (x in clusterNames) {
+  genes <- unlist(c(clusterGenePair[x]))
+  genes <- sapply(genes, function(i) i <- toString(i))
+  # print(genes)
+
+  pairCount <- as.matrix(rbind(counts[genes,]))
+  rownames(pairCount) <- genes
 
 
-W <- weightMatrix_nD(coords, span = 0.05)
+  W <- weightMatrix_nD(coords, span = 0.05)
 
-wcor <- as.matrix(sapply(1:nrow(coords), function(i) corTaylor(pairCount, W[i, ])))
+  wcor <- as.matrix(sapply(1:nrow(coords), function(i) corTaylor(pairCount, W[i, ])))
 
-set.seed(500)
-nitr = 1000
-pwcor <- matrix(nrow = nitr, ncol = nrow(coords))
+  set.seed(500)
+  nitr = 1000
 
-pwcor <- sapply(1:nitr, function(i) {
-  x <- pairCount
-  o = sample(1:nrow(coords))
-  x <- t(sapply(1:nrow(pairCount), function(j) {
-    x[j,] = pairCount[j,o]
+  pwcor <- matrix(nrow = nitr, ncol = nrow(coords))
+  pwcor <- sapply(1:nitr, function(i) {
+    x <- pairCount
+    o = sample(1:nrow(coords))
+    x <- t(sapply(1:nrow(pairCount), function(j) {
+        x[j,] = pairCount[j,o]
+    }))
+
+    pwcor[i,] = sapply(1:nrow(W), function(j) corTaylor(x, W[j, ]))
+  })
+
+  pvals <- matrix(nrow = nrow(coords), ncol = 1)
+  pvals <- as.matrix(sapply(1:nrow(wcor), function(i) {
+    pvals[i,] = sum(pwcor[i,] > wcor[i])/nitr
   }))
 
-  pwcor[i,] = sapply(1:nrow(W), function(j) corTaylor(x, W[j, ]))
-})
 
-pvals <- matrix(nrow = nrow(coords), ncol = 1)
-pvals <- as.matrix(sapply(1:nrow(wcor), function(i) {
-  pvals[i,] = sum(pwcor[i,] > wcor[i])/nitr
-}))
+  df_res <- data.frame(x = coords[,"x"],
+                       y = coords[,"y"],
+                       wcor = wcor,
+                       pvals = pvals)
 
+  pdf(paste0("output/p_val_plots/", x, ".pdf"),
+      height = 4.5, width = 12,onefile=TRUE)
+  print(plot_wcor(df_res))
+  print(plot_pvals(df_res))
+  dev.off()
 
-
-## Plot the P-values
-
-require(ggforce)
-require(patchwork)
-require(ggpubr)
-
-df_res <- data.frame(x = coords[,"x"],
-                     y = coords[,"y"],
-                     wcor = wcor,
-                     pvals = pvals)
-
-t = theme(legend.key.width = unit(0.5, "inches")) +
-  theme(plot.title = element_text(size = 20)) +
-  theme(axis.title = element_text(size = 15))
-
-# plot_this <- ggplot(df_res, aes(x = -x, y = y, fill = pvals)) +
-#   geom_voronoi_tile(max.radius = 5) +
-#   theme_minimal() +
-#   theme(panel.grid = element_blank()) +
-#   theme(axis.text = element_blank()) +
-#   theme(plot.title = element_text(hjust = 0.5)) +
-#   theme(legend.position = "bottom") +
-#   labs(colour = "",fill = "") +
-#   labs(colour = "local correlation",fill = "local correlation") +
-#   ylab("") +
-#   xlab("") +
-#   ggtitle("correlation of both genes") +
-#   scale_alpha_continuous(range = c(0,0.5)) +
-#   scale_fill_gradient2(low = "blue", mid = "white", high = "red", limits = c(0,1)) +
-#   t +
-#   coord_fixed() +
-#   guides(fill = guide_colourbar(title.position = "top",
-#                                 title.hjust = 0.5)) +
-#   theme(legend.title=element_text(size=15)) +
-#   NULL
-
-plot_wcor <- ggplot(df_res, aes(x = -x, y = y)) +
-  geom_point(aes(colour = wcor), size = 5) +
-  theme_minimal() +
-  theme(panel.grid = element_blank()) +
-  theme(axis.text = element_blank()) +
-  xlab("") +
-  ylab("") +
-  labs(colour = "") +
-  theme(legend.position = "bottom") +
-  theme(plot.title = element_text(hjust = 0.5, face = "italic")) +
-  scale_color_viridis_c(breaks = c(0,max(df_res$wcor)),
-                        limits = c(0,max(df_res$wcor)),
-                        labels = c("Low","High")) +
-  t +
-  coord_fixed() +
-  guides(colour = guide_colourbar(title.position = "top",
-                                  title.hjust = 0.5)) +
-  theme(legend.title=element_text(size=15)) +
-  labs(colour = "Weighted Correlation") +
-  NULL
-
-plot_pvals <- ggplot(df_res, aes(x = -x, y = y)) +
-  # geom_point(aes(colour = pvals), size = 5) +
-  geom_point(aes(colour = -log10(pvals)), size = 5) +
-  theme_minimal() +
-  theme(panel.grid = element_blank()) +
-  theme(axis.text = element_blank()) +
-  xlab("") +
-  ylab("") +
-  labs(colour = "") +
-  theme(legend.position = "bottom") +
-  theme(plot.title = element_text(hjust = 0.5, face = "italic")) +
-  scale_color_viridis_c(breaks = c(0,max(df_res$pvals)),
-                        limits = c(0,max(df_res$pvals)),
-                        labels = c("Low","High")) +
-  t +
-  coord_fixed() +
-  guides(colour = guide_colourbar(title.position = "top",
-                                  title.hjust = 0.5)) +
-  theme(legend.title=element_text(size=15)) +
-  labs(colour = "-log(pval)") +
-  NULL
+  # break
+}
