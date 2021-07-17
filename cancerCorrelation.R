35f164ed6f2e3809f808c221d1df86f9f8eb30f7
Enan Ajmain <3nan.ajmain@gmail.com>
2021-07-17 11:42:32 +0600

    Use R's scale function to compute Z-score
    Instead of manually computing it. Why use personal function that may bug
    out at corner cases when there is a perfectly good library function.

--- cancerCorrelation.R
+++ cancerCorrelation.R
@@ -71,17 +71,17 @@ clusterNames <- sapply(clusterNames, function(i) i <- toString(i))
 clusterGenes <- clusterData[,8]
 clusterGenePair <- list()
 if (length(clusterNames) == 1) {
-  clusterGenePair[[clusterNames[1]]] <- clusterData[clusterData[,7] == clusterNames[1], 8]
+    clusterGenePair[[clusterNames[1]]] <- clusterData[clusterData[,7] == clusterNames[1], 8]
 } else {
-  clusterGenePair <- sapply(clusterNames, function(i) {
+    clusterGenePair <- sapply(clusterNames, function(i) {
         clusterGenePair[[i]] <- clusterData[clusterData[,7] == i, 8]
   })
 }
 
 
-clusterGenePair[["MyeloidFibroblast"]] = unlist( (  list(clusterGenePair[["Myeloid"]], clusterGenePair[["Fibroblast"]])) )
-clusterGenePair[["EpithelialFibroblast"]] = unlist( (  list(clusterGenePair[["Epithelial"]], clusterGenePair[["Fibroblast"]])) )
-clusterGenePair[["MyeloidEpithelialt"]] = unlist( (  list(clusterGenePair[["Myeloid"]], clusterGenePair[["Epithelial"]])) )
+clusterGenePair[["MyeloidFibroblast"]] = unlist((list(clusterGenePair[["Myeloid"]], clusterGenePair[["Fibroblast"]])) )
+clusterGenePair[["EpithelialFibroblast"]] = unlist((list(clusterGenePair[["Epithelial"]], clusterGenePair[["Fibroblast"]])) )
+clusterGenePair[["MyeloidEpithelialt"]] = unlist((list(clusterGenePair[["Myeloid"]], clusterGenePair[["Epithelial"]])) )
 
 
 
@@ -91,162 +91,164 @@ if (!file.exists("output/pval_plots_cancer")) {
 
 plotdf = function(df_res, vals1, vals2, label1, label2) {
 
-  # for (i in 1:length(vals1)) {
-  #   if (vals2[i] > 0.5) {
-  #     vals1[i] <- NA
-  #   }
-  # }
+    # for (i in 1:length(vals1)) {
+    #   if (vals2[i] > 0.5) {
+    #     vals1[i] <- NA
+    #   }
+    # }
 
-  plot_vals1 <- ggplot(df_res, aes(x = x, y = -y)) +
-    geom_point(aes(colour = vals1), size = 5) +
-    theme_minimal() +
-    theme(panel.grid = element_blank()) +
-    theme(axis.text = element_blank()) +
-    xlab("") +
-    ylab("") +
-    labs(colour = "") +
-    theme(legend.position = "bottom") +
-    theme(plot.title = element_text(hjust = 0.5, face = "italic")) +
-    scale_color_viridis_c(na.value = "black", option = "plasma") +
-    coord_fixed() +
-    guides(colour = guide_colourbar(title.position = "top",
-                                    title.hjust = 0.5)) +
-    theme(legend.key.width = unit(0.5, "inches")) +
-    theme(plot.title = element_text(size = 20)) +
-    theme(axis.title = element_text(size = 15)) +
-    theme(legend.title = element_text(size = 15)) +
-    labs(colour = label1) +
-    NULL
+    plot_vals1 <- ggplot(df_res, aes(x = x, y = -y)) +
+        geom_point(aes(colour = vals1), size = 5) +
+        theme_minimal() +
+        theme(panel.grid = element_blank()) +
+        theme(axis.text = element_blank()) +
+        xlab("") +
+        ylab("") +
+        labs(colour = "") +
+        theme(legend.position = "bottom") +
+        theme(plot.title = element_text(hjust = 0.5, face = "italic")) +
+        scale_color_viridis_c(option = "plasma") +
+        coord_fixed() +
+        guides(colour = guide_colourbar(title.position = "top",
+                                        title.hjust = 0.5)) +
+        theme(legend.key.width = unit(0.5, "inches")) +
+        theme(plot.title = element_text(size = 20)) +
+        theme(axis.title = element_text(size = 15)) +
+        theme(legend.title = element_text(size = 15)) +
+        labs(colour = label1) +
+        NULL
 
-  plot_vals2 <- ggplot(df_res, aes(x = x, y = -y)) +
-    # geom_point(aes(colour = vals2), size = 5) +
-    geom_point(aes(colour = -log10(vals2)), size = 5) +
-    theme_minimal() +
-    theme(panel.grid = element_blank()) +
-    theme(axis.text = element_blank()) +
-    xlab("") +
-    ylab("") +
-    labs(colour = "") +
-    theme(legend.position = "bottom") +
-    theme(plot.title = element_text(hjust = 0.5, face = "italic")) +
-    scale_color_viridis_c(option = "plasma") +
-    coord_fixed() +
-    guides(colour = guide_colourbar(title.position = "top",
-                                    title.hjust = 0.5)) +
-    theme(legend.key.width = unit(0.5, "inches")) +
-    theme(plot.title = element_text(size = 20)) +
-    theme(axis.title = element_text(size = 15)) +
-    theme(legend.title = element_text(size = 15)) +
-    labs(colour = label2) +
-    NULL
+    plot_vals2 <- ggplot(df_res, aes(x = x, y = -y)) +
+        geom_point(aes(colour = vals2), size = 5) +
+        # geom_point(aes(colour = -log10(vals2)), size = 5) +
+        theme_minimal() +
+        theme(panel.grid = element_blank()) +
+        theme(axis.text = element_blank()) +
+        xlab("") +
+        ylab("") +
+        labs(colour = "") +
+        theme(legend.position = "bottom") +
+        theme(plot.title = element_text(hjust = 0.5, face = "italic")) +
+        scale_color_viridis_c(option = "plasma") +
+        coord_fixed() +
+        guides(colour = guide_colourbar(title.position = "top",
+                                        title.hjust = 0.5)) +
+        theme(legend.key.width = unit(0.5, "inches")) +
+        theme(plot.title = element_text(size = 20)) +
+        theme(axis.title = element_text(size = 15)) +
+        theme(legend.title = element_text(size = 15)) +
+        labs(colour = label2) +
+        NULL
 
-  vals1_leg = as_ggplot(get_legend(plot_vals1))
-  vals2_leg = as_ggplot(get_legend(plot_vals2))
+    vals1_leg = as_ggplot(get_legend(plot_vals1))
+    vals2_leg = as_ggplot(get_legend(plot_vals2))
 
-  scater::multiplot(plot_vals1 + theme(legend.position = "none")
-                    + theme(plot.margin = margin(10,0,-10,0)),
-                    plot_vals2 + theme(legend.position = "none")
-                    + theme(plot.margin = margin(10,0,-10,0)),
-                    vals1_leg, vals2_leg,
-                    layout = matrix(
-                      c(1,1,1,2,2,2,
-                        1,1,1,2,2,2,
-                        1,1,1,2,2,2,
-                        3,3,3,4,4,4), ncol = 6, byrow = TRUE))
+    scater::multiplot(plot_vals1 + theme(legend.position = "none")
+                      + theme(plot.margin = margin(10,0,-10,0)),
+                      plot_vals2 + theme(legend.position = "none")
+                      + theme(plot.margin = margin(10,0,-10,0)),
+                      vals1_leg, vals2_leg,
+                      layout = matrix(c(1,1,1,2,2,2,
+                                        1,1,1,2,2,2,
+                                        1,1,1,2,2,2,
+                                        3,3,3,4,4,4),
+                                      ncol = 6,
+                                      byrow = TRUE))
 }
 
 
 # W <- weightMatrix_nD(coords, span = 0.3)
 W <- weightMatrix_gaussian(coords, l = 0.5)
+
 clusterNames = append(clusterNames,"EpithelialFibroblast" ,length(clusterNames))
 clusterNames = append(clusterNames,"MyeloidFibroblast" ,length(clusterNames))
 clusterNames = append(clusterNames,"MyeloidEpithelialt" ,length(clusterNames))
 
 
 for (x in clusterNames) {
+    if (!(x=="Epithelial" | x=="Fibroblast" | x=="Myeloid")) next
+    if (!(x=="Epithelial")) next
 
-  # if (!(x=="Epithelial" | x=="Fibroblast" | x=="Myeloid" | x=="MyeloidFibroblast" | x=="EpithelialFibroblast" | x=="MyeloidEpithelialt")) next
-  if (!(x=="MyeloidFibroblast" )) next
+    # if (!(x=="MyeloidFibroblast" | x=="EpithelialFibroblast" | x=="MyeloidEpithelialt")) next
+    # if (!(x=="MyeloidFibroblast" )) next
 
-  genes <- unlist(c(clusterGenePair[x]))
-  genes <- sapply(genes, function(i) i <- toString(i))
-  # genes = sort(genes)
-  # genes = genes[1:2]
-  if (length(genes) == 1) next
+    genes <- unlist(c(clusterGenePair[x]))
+    genes <- sapply(genes, function(i) i <- toString(i))
+    # genes = sort(genes)
+    # genes = genes[1:2]
+    if (length(genes) == 1) next
 
-  print(x)
-  print(genes)
+    print(x)
+    print(genes)
 
-  pairCount <- as.matrix(rbind(counts[genes,]))
-  rownames(pairCount) <- genes
+    pairCount <- as.matrix(rbind(counts[genes,]))
+    rownames(pairCount) <- genes
 
-  st = Sys.time()
-  meanOfZenes = c()
-  sdOfZenes = c()
-  for(itr in 1:nrow(pairCount)) {
-    meanOfZenes = append(meanOfZenes, mean(pairCount[itr,]), length(meanOfZenes))
-    sdOfZenes = append(sdOfZenes, sd(pairCount[itr,]), length(sdOfZenes))
-  }
-  zscr <- as.matrix(sapply(1:nrow(coords),
-                           function(i) zScore(pairCount, i, meanOfZenes, sdOfZenes)))
-  et = Sys.time()
-  message("Runtime of Z-score: ", et-st)
-
-  st = Sys.time()
-  meig <- as.matrix(sapply(1:nrow(coords),
-                           function(i) maxEigenVal(pairCount, W[i,])))
-  et = Sys.time()
-  message("Runtime of eigenvalues: ", et-st)
-
-  message(paste0("Conducting permutation tests for ", x))
-
-  set.seed(500)
-  nitr = 1000
-
-  # pwcor <- matrix(nrow = nitr, ncol = nrow(coords))
-  # pwcor <- sapply(1:nitr, function(i) {
-  #   x <- pairCount
-  #   o = sample(1:nrow(coords))
-  #   x <- t(sapply(1:nrow(pairCount), function(j) {
-  #       x[j,] = pairCount[j,o]
-  #   }))
-  #   pwcor[i,] = sapply(1:nrow(W), function(j) corTaylor(x, W[j, ]))
-  # })
-
-  pmeig <- matrix(nrow = nitr, ncol = nrow(coords))
-  pmeig <- sapply(1:nitr, function(i) {
-    print(i)
-    x <- pairCount
-    o = sample(1:nrow(coords))
-    x <- t(sapply(1:nrow(pairCount), function(j) {
-        x[j,] = pairCount[j,o]
+    st = Sys.time()
+    zscr <- as.matrix(sapply(1:nrow(coords),
+                             function(i) {
+                                 zscores <- apply(pairCount, 1, scale)
+                                 aggzscores <- sum(zscores[i,])/nrow(pairCount)
     }))
-    pmeig[i,] = sapply(1:nrow(W), function(j) maxEigenVal(x, W[j, ]))
-  })
+    et = Sys.time()
+    message("Runtime of Z-score: ", et-st)
 
-  # pvals_cor <- matrix(nrow = nrow(coords), ncol = 1)
-  # pvals_cor <- as.matrix(sapply(1:nrow(wcor), function(i) {
-  #   pvals_cor[i,] = (sum(pwcor[i,] > wcor[i]) + 1) / (nitr + 1)
-  # }))
-  pvals_eig <- matrix(nrow = nrow(coords), ncol = 1)
-  pvals_eig <- as.matrix(sapply(1:nrow(meig), function(i) {
-    pvals_eig[i,] = (sum(pmeig[i,] > meig[i]) + 1) / (nitr + 1)
-  }))
+    st = Sys.time()
+    meig <- as.matrix(sapply(1:nrow(coords),
+                             function(i) maxEigenVal(pairCount, W[i,])))
+    et = Sys.time()
+    message("Runtime of eigenvalues: ", et-st)
+
+    # message(paste0("Conducting permutation tests for ", x))
+
+    # set.seed(500)
+    # nitr = 1000
+
+    # pwcor <- matrix(nrow = nitr, ncol = nrow(coords))
+    # pwcor <- sapply(1:nitr, function(i) {
+    #   x <- pairCount
+    #   o = sample(1:nrow(coords))
+    #   x <- t(sapply(1:nrow(pairCount), function(j) {
+    #       x[j,] = pairCount[j,o]
+    #   }))
+    #   pwcor[i,] = sapply(1:nrow(W), function(j) corTaylor(x, W[j, ]))
+    # })
+
+    # pmeig <- matrix(nrow = nitr, ncol = nrow(coords))
+    # pmeig <- sapply(1:nitr, function(i) {
+    #   print(i)
+    #   x <- pairCount
+    #   o = sample(1:nrow(coords))
+    #   x <- t(sapply(1:nrow(pairCount), function(j) {
+    #       x[j,] = pairCount[j,o]
+    #   }))
+    #   pmeig[i,] = sapply(1:nrow(W), function(j) maxEigenVal(x, W[j, ]))
+    # })
+
+    # pvals_cor <- matrix(nrow = nrow(coords), ncol = 1)
+    # pvals_cor <- as.matrix(sapply(1:nrow(wcor), function(i) {
+    #   pvals_cor[i,] = (sum(pwcor[i,] > wcor[i]) + 1) / (nitr + 1)
+    # }))
+    # pvals_eig <- matrix(nrow = nrow(coords), ncol = 1)
+    # pvals_eig <- as.matrix(sapply(1:nrow(meig), function(i) {
+    #   pvals_eig[i,] = (sum(pmeig[i,] > meig[i]) + 1) / (nitr + 1)
+    # }))
 
 
-  df_res <- data.frame(x = coords[,"x"],
-                       y = coords[,"y"],
-                      #  zscr = zscr,
-                       meig = meig,
-                       pval =pvals_eig)
+    df_res <- data.frame(x = coords[,"x"],
+                         y = coords[,"y"],
+                         zscr = zscr,
+                         meig = meig)
+                         # meig = meig,
+                         # pval =pvals_eig)
 
-  pdf(paste0("output/pval_plots_cancer/", x, ".pdf"),
-      height = 6, width = 10, onefile = F)
-  # plotdf(df_res,df_res$wcor,df_res$pvals_cor,"Weighted Correlation", "-log10(pval)")
-  # plotdf(df_res,df_res$zscr,df_res$meig,"Z-score", "Largest Eigenvalue")
-  plotdf(df_res,df_res$meig,df_res$pval,"Largest Eigenvalue","-log10(pval)")
+    pdf(paste0("output/pval_plots_cancer/", x, ".pdf"),
+        height = 6, width = 10, onefile = F)
+    plotdf(df_res,df_res$zscr,df_res$meig,"Z-score", "Largest Eigenvalue")
+    # plotdf(df_res,df_res$wcor,df_res$pvals_cor,"Weighted Correlation", "-log10(pval)")
+    # plotdf(df_res,df_res$meig,df_res$pval,"Largest Eigenvalue","-log10(pval)")
 
-  dev.off()
+    dev.off()
 
-  # break
+    # break
 }
