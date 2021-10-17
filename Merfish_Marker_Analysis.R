3f60a36448b441fb9dab7e026e651a721c295476
Enan Ajmain <3nan.ajmain@gmail.com>
2021-10-17 14:47:35 +0600

    Update intermediate MERFISH analysis scripts

---
+++ Merfish_Marker_Analysis.R
@@ -0,0 +1,43 @@
+#  ======================================================================
+#                                  load libraries
+#  ======================================================================
+
+library(SingleCellExperiment)
+library(dplyr)
+library(Seurat)
+library(patchwork)
+
+
+#  ======================================================================
+#                                  Preprocessing
+#  ======================================================================
+
+dataset = read.csv(file="./datasets/moffitt/s7.csv")
+
+counts_raw = dataset[,-c(1:5, 8,9)]
+counts = counts_raw[, colSums(counts_raw) != 0]
+
+coords = -1 * counts[,c(1,2)]
+colnames(coords) = c("x", "y")
+rownames(coords) = apply(coords, 1, function(i) {
+   paste0(i[1], "x", i[2])
+})
+
+rownames(counts) = rownames(coords)
+counts = counts[,-c(1,2)]
+
+
+sce = SingleCellExperiment(assays = list(counts = t(counts)), colData = coords)
+sce = logNormCounts(sce)
+counts = logcounts(sce)
+
+
+
+
+plot(counts[,4])
+
+genes = colnames(counts)[-c(1,2,3)]
+message("Number of genes: ", length(genes))
+
+clusters = unique(counts[,3])
+message("Number of clusters: ", length(clusters))
--- merfish.R
+++ merfish.R
@@ -1,21 +1,21 @@
 library("readxl")
-
-cr = read_xlsx("./datasets/moffitt/s7.xlsx")
-
+cr = read.csv(file="./datasets/moffitt/s7.csv")
 
 z = c()
 for (x in cr$Cell_class) {
     # print(x)
     if      (x=="Inhibitory")   z = append(z,2)
-    # else if (x=="Excitatory")   z = append(z,2)
-    # else if (x=="Mature OD")    z = append(z,3)
-    # else if (x=="Immature OD")  z = append(z,4)
-    # else if (x=="Astrocyte")    z = append(z,5)
-    # else if (x=="Microglia")    z = append(z,6)
-    # else if (x=="Ependymal")    z = append(z,7)
-    # else if (x=="Endothelial")  z = append(z,8)
-    # else if (x=="Mural")        z = append(z,9)
+    # else if (x=="Excitatory")   z = append(z,3)
+    # else if (x=="Mature OD")    z = append(z,4)
+    # else if (x=="Immature OD")  z = append(z,5)
+    # else if (x=="Astrocyte")    z = append(z,6)
+    # else if (x=="Microglia")    z = append(z,7)
+    # else if (x=="Ependymal")    z = append(z,8)
+    # else if (x=="Endothelial")  z = append(z,9)
+    # else if (x=="Mural")        z = append(z,10)
     else                        z = append(z,1)
 }
 cr$color = z
 plot(cr$Centroid_X, cr$Centroid_Y, col=cr$color)
+
+mmc3=read.csv(file="./benchmarking_merfish/1-s2.0-S2211124718317972-mmc3.csv")
