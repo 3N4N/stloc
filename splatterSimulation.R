f62da30367fd8b392d1af5c9f14fe3a323671f11
Tousif Nibir <tousifnibir1234@gmail.com>
2021-12-15 18:26:03 +0600

    cellType division

--- splatterSimulation.R
+++ splatterSimulation.R
@@ -6,12 +6,9 @@ sce <- mockSCE()
 params <- newSplatParams()
 # counts <- counts(sce)
 # counts
-sim <- splatSimulate(params, batchCells = c(100, 100,400,25), verbose = FALSE,nGenes=1000)
+sim <- splatSimulate(params, batchCells = c(1000, 1000,1000,1000), verbose = FALSE,nGenes=1000)
 
-# sim.groups <- splatSimulate(group.prob = c(0.3, 0.5,0.2), method = "groups",
-#                             verbose = FALSE)
-# sim.groups <- normalize(sim.groups)
-# sim.groups
+
 sim <- logNormCounts(sim)
 simPCA <- runPCA(sim)
 mat<- counts(sim)
@@ -24,3 +21,34 @@ r<-rownames(mat)
 write.csv(df_mtx, file = "splatterCSV.csv", append = FALSE, quote = TRUE, sep = ",")
 
 plotPCA(simPCA, colour_by = "Batch")
+
+cellTypes <-as.list(unique(df_mtx["cellType"]))
+
+typeWiseDf<- c()
+for ( type in cellTypes)
+{
+    x<-df_mtx[df_mtx["cellType"]==type,]
+    typeWiseDf<- append(typeWiseDf,x)
+}
+for ( i in 1:20)
+{
+    for( j in 1:20)
+    {
+        if( 1<=i &&  i <=10 && 1<=j && j<=10)
+        {
+
+            "cellTypeA"
+            
+        }
+        else if (11<= i  &&  i <=20  && 1<=j && j<=10) {
+           "cellTypeB"
+        }
+        else if (1<= i  && i <=10  &&  11<=j && j<=20) {
+           "cellTypeC"
+        }
+        else if(11<=i && i<=20  &&  11<=j && j<=20){
+            "cellType D"
+        }
+
+    }
+}
