fd4586f2919d217a203d04efa47101c5c576102c
Enan Ajmain <3nan.ajmain@gmail.com>
2021-12-23 11:18:08 +0600

    Action: add ks to list of required packages

--- install_packages.R
+++ install_packages.R
@@ -2,7 +2,7 @@ if (!requireNamespace("BiocManager", quietly = TRUE))
     install.packages("BiocManager")
 
 
-packages.cran = c("ggplot2", "gridExtra")
+packages.cran = c("ggplot2", "gridExtra", "ks")
 packages.bioc = c("SingleCellExperiment", "scran")
 
 install.packages(packages.cran, method="wget")
