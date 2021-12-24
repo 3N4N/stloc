e38ecac4573fc0035d40d96ec1a84bed5d3ce7d7
Enan Ajmain <3nan.ajmain@gmail.com>
2021-12-24 20:45:52 +0600

    SL: create X and y to train the models with

--- merfish_modelling.py
+++ merfish_modelling.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-import sklearn as skl
+# import sklearn as skl
 
 df = pd.read_csv("data/merfish/merfishSpatial.csv", sep=" ")
 df.drop(columns=['coord'], inplace=True)
@@ -10,8 +10,22 @@ df.head()
 
 reference = pd.read_csv("data/merfish/markerGene_for_merfish_data.csv")
 reference.drop(columns=['p_value'], inplace=True)
+reference.drop(range(148,168))
+reference.drop(reference.index[reference.cell_type=='EpendymalInhibitory'].tolist(), inplace=True)
 reference['cell_type'] = reference['cell_type'].astype(str).str.replace(" ", "")
-markers = reference.groupby('cell_type').agg(list).marker_gene
-celltypes = list(reference.cell_type.unique())
 
-_df = df[markers['Astrocyte'] + ['Astrocyte']]
+markers = reference.groupby('cell_type').agg(list).marker_gene
+celltypes = reference.cell_type.unique().tolist()
+expressions = df.drop(columns=celltypes)
+
+_X = []
+
+for celltype in celltypes:
+    _expressions = expressions
+    _expressions[[i for i in _expressions.columns if i not in markers[celltype]]] = 0
+    _df = pd.concat([_expressions,df[celltype]], axis=1)
+    _df.rename(columns={celltype:'cellcount'}, inplace=True)
+    _X.append(_df)
+
+X = pd.concat(_X, ignore_index = True)
+X.info()
