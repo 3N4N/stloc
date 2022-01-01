41b0ff1b22f988d8ab5857b6b9d7aa0a67e7509c
Enan Ajmain <3nan.ajmain@gmail.com>
2022-01-01 22:43:03 +0600

    SL: save R2-score and Spearman's correlations

--- merfish_modelling.py
+++ merfish_modelling.py
@@ -1,15 +1,23 @@
+# REGRESSION MODELLING ON MERFISH DATASET
+#
+# NOTE: use spearman
+
 import pandas as pd
 import numpy as np
-
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.metrics import accuracy_score
-
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.preprocessing import StandardScaler, MinMaxScaler
+from sklearn import metrics
+from scipy import stats
 
-
+plt.rcParams['figure.figsize'] = [8,5]
+plt.rcParams['font.size'] =14
+plt.rcParams['font.weight']= 'bold'
+plt.style.use('seaborn-whitegrid')
 
 
 df = pd.read_csv("data/merfish/merfishSpatial.csv", sep=" ")
@@ -28,41 +36,58 @@ markers = reference.groupby('cell_type').agg(list).marker_gene
 celltypes = reference.cell_type.unique().tolist()
 expressions = df.drop(columns=celltypes)
 
-scaler = StandardScaler()
-expressionscaled = scaler.fit_transform(expressions)
+xcel = None #np.array([[1,1,1,1,1]])
 
-# _df = []
+for celltype in celltypes:
 
-# for celltype in celltypes:
-#     _expressions = expressions
-#     _expressions[[i for i in _expressions.columns if i not in markers[celltype]]] = 0
-#     __df = pd.concat([_expressions,df[celltype]], axis=1)
-#     __df.rename(columns={celltype:'cellcount'}, inplace=True)
-#     _df.append(__df)
+    row = [celltype, len(markers[celltype])]
 
-# _df = pd.concat(_df, ignore_index = True)
+    _df = pd.concat([expressions[markers[celltype]], df[celltype]], axis=1)
+    _df.rename(columns={celltype:'cellcount'}, inplace=True)
 
-# _df.info()
-# _df.describe()
-# _df.groupby('cellcount').size()
+    X = _df.drop(columns=['cellcount'])
+    y = _df['cellcount']
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
-# _df.hist()
-# plt.show()
+    scaler = StandardScaler()
+    X_train = scaler.fit_transform(X_train)
+    X_test = scaler.transform(X_test)
 
+    model = LinearRegression()
+    model.fit(X_train, y_train)
+    yhat = model.predict(X_test)
+    y_pred = np.round(yhat)
+    y_pred = yhat
 
-celltype = "Astrocyte"
+    # np.vstack((y_pred,y_test)).T
 
-_expressions = expressions[markers[celltype]]
-_df = pd.concat([_expressions,df[celltype]], axis=1)
+    r2_lr = round(metrics.r2_score(y_test,y_pred), 2)
+    sp_lr = round(stats.spearmanr(y_test,y_pred).correlation, 2)
 
-X = _df.drop(columns=[celltype])
-y = _df[celltype]
+    model = RandomForestRegressor()
+    model.fit(X_train, y_train)
+    yhat = model.predict(X_test)
+    y_pred = np.round(yhat)
+    y_pred = yhat
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+    # np.vstack((y_pred,y_test)).T
 
+    r2_rf = round(metrics.r2_score(y_test,y_pred), 2)
+    sp_rf = round(stats.spearmanr(y_test,y_pred).correlation, 2)
 
-model = DecisionTreeClassifier()
-model.fit(X_train, y_train)
-yhat = model.predict(X_test)
+    # print("--------", celltype, "--------")
+    # print('R^2 Score(LR):', r2_lr)
+    # print('Correlation(LR):', sp_lr)
+    # print('R^2 Score(rf):', r2_rf)
+    # print('Correlation(rf):', sp_rf)
 
-accuracy_score(y_test, yhat)
+    row = np.append(row, [r2_lr, sp_lr, r2_rf, sp_rf])
+    # print(row, type(row))
+    if xcel is None:
+        xcel = np.array([row])
+    else:
+        xcel = np.concatenate((xcel, [row]))
+
+xcel = pd.DataFrame(xcel, columns=['celltype', 'n_markers', 'R2(LR)', 'Spearman(LR)', 'R2(RF)', 'Spearman(RF)'])
+print(xcel)
+xcel.to_csv("output/merfish/merfish_model.csv", sep=',')
