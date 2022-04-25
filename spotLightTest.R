edc7dba87b2e3cef0e738899cf747883305cf8ab
Tousif Nibir <tousifnibir1234@gmail.com>
2022-04-25 15:59:40 +0600

    spotlight test

---
+++ spotLightTest.R
@@ -0,0 +1,49 @@
+library(ggplot2)
+library(SPOTlight)
+library(SingleCellExperiment)
+library(SpatialExperiment)
+library(scater)
+library(scran)
+library(NMF)
+library(nnls)
+
+
+for (f in list.files("R/")) {
+    source(paste0("R/", f))
+}
+
+sce <- mockSC(ng = 200, nc = 10, nt = 3)
+spe <- mockSP(sce)
+mgs <- getMGS(sce)
+
+singleCellFile <- read.table("./data/twoType/allSingleCell/AmbiguousEndothelial1Single.csv", sep = ",", header = TRUE, check.names = FALSE)
+coldata <- DataFrame(
+    type = singleCellFile[, "type"]
+)
+singleCellFile <- subset(singleCellFile, select = -c(type))
+singleCellFile <- t(singleCellFile)
+singleCellData <- SingleCellExperiment(list(counts = singleCellFile))
+colData(singleCellData) <- coldata
+
+markers <- getMGS(singleCellData)
+spatialCellFile <- read.table("./data/twoType/spatialData/AmbiguousEndothelial1Spatial.csv", sep = ",", header = TRUE, check.names = FALSE)
+
+coord <- spatialCellFile[, "coord"]
+
+metaData <- spatialCellFile[(length(spatialCellFile) - 3):length(spatialCellFile)]
+spatialCellFile <- subset(spatialCellFile, select = -c(coord))
+spatialCellFile <- subset(spatialCellFile, select = -c((length(spatialCellFile) - 3):length(spatialCellFile)))
+spatialCellFile <- t(spatialCellFile)
+spatialData <- SingleCellExperiment((list(counts = spatialCellFile)))
+colnames(spatialData) <- coord
+
+res <- SPOTlight(
+    x = counts(singleCellData),
+    y = counts(spatialData),
+    groups = as.character(singleCellData$type),
+    mgs = markers,
+    hvg = NULL,
+    weight_id = "weight",
+    group_id = "type",
+    gene_id = "gene"
+)
